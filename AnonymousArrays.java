22faf01b8e06c55b3928e6b93d4127b885be6374
sahil_bambarde <sahil_bambarde>
2024-04-08 16:50:44 +0530

    Initial commit

---
+++ AnonymousArrays.java
@@ -0,0 +1,13 @@
+public class AnonymoursArrays {
+    public static void main(String[] args){
+        int[] a = new int[] {1,2,3};
+        sum(new int[] {1,2,3});
+    }
+    public static void sum(int[] s){
+        int total = 0;
+        for(int i:s){
+            total=total+i;
+        }
+        System.out.println(total);
+    }
+}
